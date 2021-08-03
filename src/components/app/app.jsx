b0ffa759dba48c3cb6b82769afdd3c7e747f1f82
AMorozov-web <AMorozov-web@users.noreply.github.com>
2021-08-03 14:00:13 +0300

    fix: check previous fixes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -11,7 +11,7 @@ const App = () => {
       backgroundColor={`red`}
       colors={[`green`, `blue`]}
       hoverColors={[`blue`, `green`]}
-      colorWidth={20}
+      colorWidth={10}
     />
   );
 };
